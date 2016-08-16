65cf1d2ce4714b2ddcd981dac7e02e5b0454ca60
onevcat <onevcat@gmail.com>
2016-08-16 11:12:17 +0900

    Implemented basic request

--- ProtocolNetwork/Request.swift
+++ ProtocolNetwork/Request.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+enum HTTPMethod: String {
+    case GET
+    case POST
+}
+
+protocol Request {
+    var host: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var parameter: [String: AnyObject] { get }
+    
+    associatedtype Response
+    func parse(data: Data) -> Response
+}
+
+extension Request {
+    var host: String {
+        return "http://api.onevcat.com"
+    }
+    var parameter: [String: AnyObject] {
+        return [:]
+    }
+    func send(handler: (Response?) -> Void ) {
+        let url = URL(string: host.appending(path))!
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        // request.httpBody = ???
+        
+        URLSession.shared.dataTask(with: request) {
+            data, res, error in
+            if let data = data {
+                let res = self.parse(data: data)
+                handler(res)
+            } else {
+                handler(nil)
+            }
+        }
+    }
+}
