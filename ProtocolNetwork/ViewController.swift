7474aabe9c8c172cc2f579dab7a04c69958e294b
onevcat <onevcat@gmail.com>
2016-08-16 10:59:30 +0900

    First commit

---
+++ ProtocolNetwork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProtocolNetwork
+//
+//  Created by WANG WEI on 2016/08/16.
+//  Copyright © 2016年 OneV's Den. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
